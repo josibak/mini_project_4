329efe38fc62851f2466fe7e046b3df552e6c49d
sseoy <sik8763@gmail.com>
2025-05-30 14:37:45 +0900

    test

--- miniproj_frontend/src/components/Header.jsx
+++ miniproj_frontend/src/components/Header.jsx
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="px-4 py-2 bg-gray-100 shadow-md flex justify-between items-center">
-      <Link to="/" className="text-xl font-bold">📚 Book Platform</Link>
+      <Link to="/" className="text-xl font-bold">📚 홈</Link>
+      <Link to="/books">My</Link>
       <nav className="space-x-4">
-        <Link to="/books">책 목록</Link>
-        <Link to="/write">글쓰기</Link>
+        
+        <Link to="/write">도서 추가</Link>
       </nav>
     </header>
   );
